f8fe8a4af623a305ccf64227dfdccbc7d0f909c9
Enigmatic331 <unreal_legend7@hotmail.com>
2020-09-04 01:49:44 +0800

    upload

---
+++ EthereumUtil/Forms/MainForm.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnExec = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnExec
+        '
+        Me.btnExec.Location = New System.Drawing.Point(7, 150)
+        Me.btnExec.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnExec.Name = "btnExec"
+        Me.btnExec.Size = New System.Drawing.Size(276, 32)
+        Me.btnExec.TabIndex = 0
+        Me.btnExec.Text = "Exec"
+        Me.btnExec.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(270, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "PrivateKey"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(270, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "Nonce"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 64)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(270, 20)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.Text = "gasPrice"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(294, 197)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnExec)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExec As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ EthereumUtil/Forms/MainForm.vb
@@ -0,0 +1,41 @@
+﻿Imports Nethereum.Hex.HexTypes
+Imports Nethereum.Web3
+Imports Nethereum.Hex.HexConvertors.Extensions.HexByteConvertorExtensions
+Imports Nethereum.Signer.EthereumMessageSigner
+Imports Nethereum.Util
+Imports Nethereum.HdWallet
+Imports NBitcoin
+Imports Nethereum.Hex.HexConvertors.Extensions
+Imports System.Security.Cryptography
+Imports System.Text.RegularExpressions
+
+
+Public Class MainForm
+    Private Async Sub testContract()
+
+        Dim privateKey As New Nethereum.Signer.EthECKey(TextBox1.Text)
+        Dim nonce As Integer = TextBox2.Text
+        Dim gasPrice = TextBox3.Text
+        Dim account = New Nethereum.Web3.Accounts.Account(privateKey)
+
+        Dim iweb3 = New Web3(account, "")
+
+
+        Dim signer = New Nethereum.Signer.TransactionSigner
+        Dim signedTx = signer.SignTransaction(account.PrivateKey, account.Address, 0, New System.Numerics.BigInteger(nonce), System.Numerics.BigInteger.Parse(gasPrice), New System.Numerics.BigInteger(21000))
+
+        Try
+            Dim transactionHash = Await iweb3.Eth.Transactions.SendRawTransaction.SendRequestAsync(signedTx)
+            MessageBox.Show(transactionHash)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub btnExec_Click(sender As Object, e As EventArgs) Handles btnExec.Click
+        Call testContract()
+    End Sub
+
+End Class
